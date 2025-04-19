f2d741d29cbacb7b95c0d11dc690897ad2e7eda1
yashpinder26 <yashpindersaini@gmail.com>
2025-04-19 10:03:47 +0530

    Update M2_S3P.1.cpp

--- Module2/M2_S3P.1.cpp
+++ Module2/M2_S3P.1.cpp
@@ -1,4 +1,4 @@
-#include <omp.h>
+#include <omp.h>                        // Added OpenMP library
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
@@ -7,11 +7,14 @@ using namespace std;
 using namespace std::chrono;
 
 int main() {
-  unsigned long size = 100000000;  
+  unsigned long size = 100000000;
+
+  // Changed from malloc to C++ style new
   int *v1 = new int[size];
   int *v2 = new int[size];
   int *v3 = new int[size];
 
+  // Moved random value assignment here (removed randomVector function)
   for (unsigned long i = 0; i < size; i++) {
     v1[i] = rand() % 100;
     v2[i] = rand() % 100;
@@ -19,6 +22,7 @@ int main() {
 
   auto start = high_resolution_clock::now();
 
+  // Added OpenMP parallel for loop here
   #pragma omp parallel for
   for (unsigned long i = 0; i < size; i++) {
     v3[i] = v1[i] + v2[i];
@@ -27,6 +31,7 @@ int main() {
   auto stop = high_resolution_clock::now();
   cout << "Time taken: " << duration_cast<microseconds>(stop - start).count() << " microseconds" << endl;
 
+  // Changed from free() to delete[]
   delete[] v1;
   delete[] v2;
   delete[] v3;
